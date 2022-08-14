abb22e4c3610914c831a22cb02f67abf6e0a575d
eblocha <eblocha@protonmail.com>
2022-08-14 13:32:05 -0400

    ensure prettify decorator can be called with or without parens

--- styled_logging/decorator.py
+++ styled_logging/decorator.py
@@ -7,19 +7,29 @@ from pretty_traceback.formatting import exc_to_traceback_str
 from .types import TFormatter
 
 
-def prettify(cls: Type[TFormatter], color=True, indent=4) -> Type[TFormatter]:
+def prettify(cls: Type[TFormatter] = None, /, *, color=True, indent=4) -> Type[TFormatter]:
     """Decorator to prettify a logging.Formatter exception output"""
 
-    @functools.wraps(cls, updated=())
-    class PrettyFormatter(cls):
-        def formatException(self, ei):
-            _, exc_value, traceback = ei
-            return textwrap.indent(
-                exc_to_traceback_str(exc_value, traceback, color=color), " " * indent
-            )
+    def wrap(cls: Type[TFormatter]):
+        @functools.wraps(cls, updated=())
+        class PrettyFormatter(cls):
+            def formatException(self, ei):
+                _, exc_value, traceback = ei
+                return textwrap.indent(
+                    exc_to_traceback_str(exc_value, traceback, color=color),
+                    " " * indent,
+                )
 
-        def format(self, record: logging.LogRecord):
-            record.exc_text = None
-            return super().format(record)
+            def format(self, record: logging.LogRecord):
+                record.exc_text = None
+                return super().format(record)
 
-    return PrettyFormatter
+        return PrettyFormatter
+
+    # See if we're being called as @prettify or @prettify().
+    if cls is None:
+        # We're called with parens.
+        return wrap
+
+    # We're called as @prettify without parens.
+    return wrap(cls)
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,5 @@
 from .test_context import *
+from .test_decorator import *
 from .test_formatter import *
 from .test_handlers import *
 from .test_setup import *
